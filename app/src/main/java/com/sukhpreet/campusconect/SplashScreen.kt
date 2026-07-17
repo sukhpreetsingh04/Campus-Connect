75fadffaa20919fd6563eb59d4de84d8316aa98a
sukhpreetsingh04 <sukhpreetsingh19289@gmail.com>
2026-07-17 18:00:42 +0530

    SplashScreen UI Refinement

--- app/src/main/java/com/sukhpreet/campusconect/SplashScreen.kt
+++ app/src/main/java/com/sukhpreet/campusconect/SplashScreen.kt
@@ -3,44 +3,56 @@ package com.sukhpreet.campusconect
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.sukhpreet.campusconect.ui.theme.Poppins
 
 @Composable
 fun SplashScreen() {
+
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(color = Color(0xFF023E8A)),
+            .background(
+                brush = Brush.verticalGradient(
+                    colors = listOf(
+                        Color(0xFFE3F2FD),
+                        Color(0xFF90CAF9)
+                    )
+                )
+            ),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
-            text = "Campus.connect",
-            fontFamily = FontFamily.Serif,
-            color = Color.White,
+            text = "Campus",
+            fontFamily = Poppins,
+            color = Color(0xFF1E245B),
             fontWeight = FontWeight.ExtraBold,
             fontSize = 40.sp,
-            fontStyle = FontStyle.Italic
         )
-
-        Spacer(modifier = Modifier.height(10.dp))
-
         Text(
-            text = "Connecting every student",
-            color = Color.White,
-            fontSize = 20.sp
+            text = "Connect",
+            fontFamily = Poppins,
+            style = TextStyle(
+                Brush.horizontalGradient(
+                    colors = listOf(
+                        Color(0xFF4D6FFF),
+                        Color(0xFF6A5BFF),
+                        Color(0xFFC64ED8)
+                    )
+                )
+            ),
+            fontWeight = FontWeight.ExtraBold,
+            fontSize = 40.sp
         )
     }
 }
