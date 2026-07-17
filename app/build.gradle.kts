c9276e728675727fe38cdfe948aeb017833395d2
sukhpreetsingh04 <sukhpreetsingh19289@gmail.com>
2026-07-17 17:07:31 +0530

    Some changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,16 +5,12 @@ plugins {
 
 android {
     namespace = "com.sukhpreet.campusconect"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.sukhpreet.campusconect"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
