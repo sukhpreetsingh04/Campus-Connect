04953e4def437bcdcff84640d3e0b0f5efe37665
sukhpreetsingh04 <sukhpreetsingh19289@gmail.com>
2026-07-17 17:59:46 +0530

    Added Font Family

---
+++ app/src/main/java/com/sukhpreet/campusconect/ui/theme/Font.kt
@@ -0,0 +1,12 @@
+package com.sukhpreet.campusconect.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.sukhpreet.campusconect.R
+
+val Poppins = FontFamily(
+    Font(R.font.poppins_regular, FontWeight.Normal),
+    Font(R.font.poppins_bold, FontWeight.Bold),
+    Font(R.font.poppins_extrabold, FontWeight.ExtraBold)
+)
